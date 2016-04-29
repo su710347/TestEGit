c76b07fec6d269de4dce1b5d9e6cae952e2f2a18
su710347 <879213579@qq.com>
2016-04-29 19:50:10 +0800

    github修改

--- src/com/atguigu/egit/hello/HelloEGit.java
+++ src/com/atguigu/egit/hello/HelloEGit.java
@@ -4,7 +4,7 @@ public class HelloEGit {
 	public static void main(String[] args) {
 		System.out.println("hello egit");
 		System.out.println("第二次修改");
-		
+		System.out.println("第三次修改");
 		
 	}
 }
