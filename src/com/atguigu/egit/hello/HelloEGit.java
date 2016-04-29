8046e7f5a973cc442faec801d2322f0a00842ad8
su710347 <879213579@qq.com>
2016-04-29 19:46:00 +0800

    第二次修改

--- src/com/atguigu/egit/hello/HelloEGit.java
+++ src/com/atguigu/egit/hello/HelloEGit.java
@@ -1,11 +1,10 @@
 package com.atguigu.egit.hello;
 
 public class HelloEGit {
-	
 	public static void main(String[] args) {
-		
 		System.out.println("hello egit");
+		System.out.println("第二次修改");
+		
 		
 	}
-
 }
